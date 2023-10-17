477423b163aa9b6e05288c642e8ba07dadbfca5b
Sam Fakhreddine <sfakhreddine@onica.com>
2023-10-17 13:57:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,3 +23,29 @@ setup(
         'Topic :: Utilities'
     ],
 )
+
+
+from setuptools import setup  # Prefer setuptools over distutils
+
+setup(
+    name='str2bool3',
+    packages=['str2bool3'],  
+    version='1.0.0',  
+    description='Convert string to boolean (Forked from SymonSoft/str2bool)',
+    author='Sam Fakhreddine',
+    author_email='sam.fakhreddine@gmail.com',
+    url='https://github.com/sam-fakhreddine/str2bool3',
+    download_url='https://github.com/sam-fakhreddine/str2bool3/tarball/1.0.0',  
+    keywords=['str2bool', 'bool', 'boolean', 'convert', 'yes', 'no', 'true', 'false'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Utilities'
+    ],
+)
