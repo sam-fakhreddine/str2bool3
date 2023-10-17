9e7fb699df747a43d94c493bc4f5233919c1c32d
Sam Fakhreddine <sfakhreddine@onica.com>
2023-10-17 13:58:04 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-from distutils.core import setup
-
-
-setup(
-    name='str2bool',
-    packages=['str2bool'],
-    version='1.1',
-    description='Convert string to boolean',
-    author='SymonSoft',
-    author_email='symonsoft@gmail.com',
-    url='https://github.com/symonsoft/str2bool',
-    download_url='https://github.com/symonsoft/str2bool/tarball/1.1',
-    keywords=['str2bool', 'bool', 'boolean', 'convert', 'yes', 'no', 'true', 'false'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Utilities'
-    ],
-)
-
-
 from setuptools import setup  # Prefer setuptools over distutils
 
 setup(
